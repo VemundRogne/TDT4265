33cdb19f9acb168d8546c528885a1923ae6306be
Vemund Rogne <vemundrogne@gmail.com>
2022-03-23 17:18:58 +0100

    Add some configs batch trainer

--- assignment4/SSD/batch_trainer.py
+++ assignment4/SSD/batch_trainer.py
@@ -4,5 +4,6 @@ Utility to automatically train multiple configs
 
 import train
 
-train.train("configs/ssd300_small_and_more_anchors.py", False)
-train.train("configs/ssd300_small_and_tall_anchors.py", False)
+train.train("configs/a_ssd300.py", False)
+train.train("configs/a_ssd300_small_anchors.py", False)
+train.train("configs/a_ssd300_small_and_tall_anchors.py", False)
