25abb5d8e4a55e549cf0967b82aadfb4d2c7c211
Vemund Rogne <vemundrogne@gmail.com>
2022-03-23 15:33:21 +0100

    Fix ssd300_small_anchors, inherit from ssd300

--- assignment4/SSD/configs/ssd300_small_anchors.py
+++ assignment4/SSD/configs/ssd300_small_anchors.py
@@ -1,68 +1,26 @@
-import torch
-import torchvision
-from torch.optim.lr_scheduler import MultiStepLR, LinearLR
 from ssd.modeling import SSD300, SSDMultiboxLoss, backbones, AnchorBoxes
-from tops.config import LazyCall as L
-from ssd.data.mnist import MNISTDetectionDataset
-from ssd import utils
-from ssd.data.transforms import Normalize, ToTensor, GroundTruthBoxesToAnchors
-from .utils import get_dataset_dir, get_output_dir
 
 
 from tops.config import LazyCall as L
-# The line belows inherits the configuration set for the tdt4265 dataset
-from .tdt4265 import (
+# Inherit from the ssd300 configuration
+from .ssd300 import (
     train,
+    #anchors, # We do not want to inherit the anchors, we want new ones:)
     backbone,
     loss_objective,
     model,
     optimizer,
     schedulers,
-    train_cpu_transform,
-    val_cpu_transform,
-    gpu_transform,
+    data_train,
+    data_val,
     label_map
 )
 
-data_train = dict(
-    dataset=L(MNISTDetectionDataset)(
-        data_dir=get_dataset_dir("mnist_object_detection/train"),
-        is_train=True,
-        transform=L(torchvision.transforms.Compose)(transforms=[
-            L(ToTensor)(),  # ToTensor has to be applied before conversion to anchors.
-            # GroundTruthBoxesToAnchors assigns each ground truth to anchors, required to compute loss in training.
-            L(GroundTruthBoxesToAnchors)(anchors="${anchors}", iou_threshold=0.5),
-        ])
-    ),
-    dataloader=L(torch.utils.data.DataLoader)(
-        dataset="${..dataset}", num_workers=4, pin_memory=True, shuffle=True, batch_size="${...train.batch_size}", collate_fn=utils.batch_collate,
-        drop_last=True
-    ),
-    # GPU transforms can heavily speedup data augmentations.
-    gpu_transform=L(torchvision.transforms.Compose)(transforms=[
-        L(Normalize)(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # Normalize has to be applied after ToTensor (GPU transform is always after CPU)
-    ])
-)
-data_val = dict(
-    dataset=L(MNISTDetectionDataset)(
-        data_dir=get_dataset_dir("mnist_object_detection/val"),
-        is_train=False,
-        transform=L(torchvision.transforms.Compose)(transforms=[
-            L(ToTensor)()
-        ])
-    ),
-    dataloader=L(torch.utils.data.DataLoader)(
-        dataset="${..dataset}", num_workers=4, pin_memory=True, shuffle=False, batch_size="${...train.batch_size}", collate_fn=utils.batch_collate_val
-    ),
-    gpu_transform=L(torchvision.transforms.Compose)(transforms=[
-        L(Normalize)(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-    ])
-)
 anchors = L(AnchorBoxes)(
     feature_sizes=[[38, 38], [19, 19], [10, 10], [5, 5], [3, 3], [1, 1]],
     # Strides is the number of pixels (in image space) between each spatial position in the feature map
     strides=[[8, 8], [16, 16], [32, 32], [64, 64], [100, 100], [300, 300]],
-    min_sizes=[[15, 15], [60, 60], [111, 111], [162, 162], [213, 213], [264, 264], [315, 315]],
+    min_sizes=[[20, 20], [60, 60], [111, 111], [162, 162], [213, 213], [264, 264], [315, 315]],
     # aspect ratio is defined per feature map (first index is largest feature map (38x38))
     # aspect ratio is used to define two boxes per element in the list.
     # if ratio=[2], boxes will be created with ratio 1:2 and 2:1
